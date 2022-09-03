010773a2374b4ea5e93921d486689a315c4adc40
IgnacioIraola <iraolai@hotmail.com>
2022-09-03 11:40:01 -0300

    Se agregó login

---
+++ appIA.js
@@ -0,0 +1,63 @@
+let formulario = document.getElementById("loguin");
+let formRegistro = document.getElementById("registro");
+let ingresoError = document.getElementById("Error");
+let menorEdad = document.getElementById("menoredad");
+
+// constructor
+
+class Usuario{ 
+    constructor(usuario, password, edad){
+        this.usuario = usuario;
+        this.password = password;
+        this.edad = edad;
+    }
+}
+
+
+
+let usuarios =[];
+let usuario = new Usuario("Juancarlo", "3210", "58");
+
+usuarios.push(usuario);
+
+
+formRegistro.addEventListener("submit",(e) => {
+    e.preventDefault();
+    let userLogin = document.getElementById("userRegister").value;
+    let passwordLogin = document.getElementById("passRegister").value;
+    let ageLogin = document.getElementById("ageRegister").value;
+
+    if(ageLogin < 18){
+
+        alert(`sos menor de edad no podés ingresar`)
+        console.log("menor de edad")
+    
+    }else(userLogin && passwordLogin && ageLogin);{
+        let nuevoUsuario = new Usuario (userLogin, passwordLogin, ageLogin); 
+        
+        usuarios.push(nuevoUsuario)
+        alert(`Estás registrado ${nuevoUsuario} ya podés empezar a comprar`)
+    } 
+
+
+})
+
+
+
+formulario.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let userLogin = document.getElementById("user").value;
+    let passwordLogin = document.getElementById("pass").value;
+    
+    for(let user of usuarios){
+        if(user.user == userLogin && user.password == passwordLogin){
+            alert("Bienvenido")
+            window.location.href ="index.html"
+            localStorage,setItem("usurio",user.user);
+        }else if ((user.user =! userLogin) && (user.password =! passwordLogin) || (user.user =! userLogin) || (user.password =! passwordLogin)){
+            resultado.innerHTML =`<p>Acceso denegado, datos incorrectos</p>`
+        }
+
+    }
+
+})
