c70b4e6dd3652399fa9f56f6aabdda57285508d7
IgnacioIraola <iraolai@hotmail.com>
2022-09-05 21:46:48 -0300

    Update appIA.js

--- appIA.js
+++ appIA.js
@@ -6,8 +6,8 @@ let menorEdad = document.getElementById("menoredad");
 // constructor
 
 class Usuario{ 
-    constructor(usuario, password, edad){
-        this.usuario = usuario;
+    constructor(user, password, edad){
+        this.user = user;
         this.password = password;
         this.edad = edad;
     }
@@ -32,12 +32,12 @@ formRegistro.addEventListener("submit",(e) => {
         alert(`sos menor de edad no podés ingresar`)
         console.log("menor de edad")
     
-    }else if(userLogin && passwordLogin && ageLogin >=18){
+    }else (userLogin && passwordLogin && ageLogin >=18)
         let nuevoUsuario = new Usuario (userLogin, passwordLogin, ageLogin); 
         
         usuarios.push(nuevoUsuario)
         alert(`Estás registrado ${nuevoUsuario} ya podés loguearte`)
-    } 
+    
 
 
 })
@@ -49,7 +49,13 @@ formulario.addEventListener("submit", (e) =>{
     let userLogin = document.getElementById("user").value;
     let passwordLogin = document.getElementById("pass").value;
     
-    for(let user of usuarios){
+    const ingreso = usuarios.find(user => user.user === user)
+
+    if (condition) {
+        
+    }
+
+    /*for(let user of usuarios){
         if(user.user == userLogin && user.password == passwordLogin){
             alert("Bienvenido");
             window.location.href ="index.html"
@@ -59,6 +65,6 @@ formulario.addEventListener("submit", (e) =>{
             resultado.innerHTML =`<p>Acceso denegado, datos incorrectos</p>`
         }
 
-    }
+    }*/
 
 })
