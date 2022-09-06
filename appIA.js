49e79055a6be625db3285cb2de13083faae20296
IgnacioIraola <iraolai@hotmail.com>
2022-09-06 03:05:26 -0300

    Funciona

--- appIA.js
+++ appIA.js
@@ -1,15 +1,13 @@
 let formulario = document.getElementById("login");
 let formRegistro = document.getElementById("registro");
-let ingresoError = document.getElementById("Error");
-let menorEdad = document.getElementById("menoredad");
+let ingresoError = document.getElementById("error");
 
 // constructor
 
 class Usuario{ 
-    constructor(user, password, edad){
+    constructor(user, password){
         this.user = user;
         this.password = password;
-        this.edad = edad;
     }
 }
 
@@ -25,18 +23,15 @@ formRegistro.addEventListener("submit",(e) => {
     e.preventDefault();
     let userLogin = document.getElementById("userRegister").value;
     let passwordLogin = document.getElementById("passRegister").value;
-    let ageLogin = document.getElementById("ageRegister").value;
-
-    if(ageLogin < 18){
-
-        alert(`sos menor de edad no podés ingresar`)
-        console.log("menor de edad")
-    
-    }else (userLogin && passwordLogin && ageLogin >=18)
-        let nuevoUsuario = new Usuario (userLogin, passwordLogin, ageLogin); 
+   
+    if(userLogin && passwordLogin){
+        let nuevoUsuario = new Usuario (userLogin, passwordLogin); 
         
         usuarios.push(nuevoUsuario)
-        alert(`Estás registrado ${nuevoUsuario} ya podés loguearte`)
+        alert("Estás registrado" + nuevoUsuario)
+    
+    }
+        
     
 
 
@@ -49,13 +44,10 @@ formulario.addEventListener("submit", (e) =>{
     let userLogin = document.getElementById("user").value;
     let passwordLogin = document.getElementById("pass").value;
     
-    const ingreso = usuarios.find(user => user.user === user)
+    
+    
 
-    if (condition) {
-        
-    }
-
-    /*for(let user of usuarios){
+    for(let user of usuarios){
         if(user.user == userLogin && user.password == passwordLogin){
             alert("Bienvenido");
             window.location.href ="index.html"
@@ -65,6 +57,6 @@ formulario.addEventListener("submit", (e) =>{
             resultado.innerHTML =`<p>Acceso denegado, datos incorrectos</p>`
         }
 
-    }*/
+    }
 
 })
