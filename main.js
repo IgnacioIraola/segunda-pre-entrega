dce22b25dea357e4b6643fc8fa3152089fd8434c
IgnacioIraola <iraolai@hotmail.com>
2022-09-08 19:25:48 -0300

    Modificaciones

---
+++ main.js
@@ -0,0 +1,73 @@
+let formulario = document.getElementById("login");
+let formRegistro = document.getElementById("registro");
+let edad = document.getElementById("edad");
+//let ingresoError = document.getElementById("error");
+
+// constructor
+
+class Usuario{ 
+    constructor(user, password, edad){
+        this.user = user;
+        this.password = password;
+        this.edad = edad;
+    }
+}
+
+
+
+let usuarios =[];
+let usuario = new Usuario("Juancarlo", "3210", "58");
+
+
+
+usuarios.push(usuario);
+
+
+formRegistro.addEventListener("submit",(e) => {
+    e.preventDefault();
+    let userLogin = document.getElementById("userRegister").value;
+    let passwordLogin = document.getElementById("passRegister").value;
+    let edadLogin = document.getElementById("edadRegister").value;
+    
+
+    const regPermiso = (edadLogin >=18) ? true : false
+
+    regPermiso ? swal() : swal ("Sos menor  no podes ingresar")
+   
+
+    if (edadLogin >=18 ) {
+        if(userLogin && passwordLogin){
+            let nuevoUsuario = new Usuario (userLogin, passwordLogin); 
+    
+            usuarios.push(nuevoUsuario)
+            swal(`Estás registrado ${userLogin}, podés loguearte para comprar`)
+        
+        }
+
+    }
+    
+        
+
+})
+
+
+
+formulario.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let userLogin = document.getElementById("user").value;
+    let passwordLogin = document.getElementById("pass").value;
+    
+    
+    
+    if(user.user == userLogin && user.password == passwordLogin){
+        alert("Bienvenido");
+        window.location.href ="index.html"
+        localStorage.setItem("usuario",user.user);
+    }else if ((user.user =! userLogin) && (user.password =! passwordLogin) || 
+        (user.user =! userLogin) || (user.password =! passwordLogin)){
+            resultado.innerHTML =`<p>Acceso denegado, datos incorrectos</p>`
+    }
+
+    
+
+})
